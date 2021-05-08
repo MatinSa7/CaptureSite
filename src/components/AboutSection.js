d380b155b53acb7f64e50d1702b6baad2b3c926f
Matin Sarahi <matinsarahi@gmail.com>
2021-05-08 16:58:43 -0400

    styling componenets about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,29 +5,29 @@ import styled from "styled-components";
 const AboutSection = () => {
   return (
     <StyledAbout className="about">
-      <div className="description">
+      <StyledDescription>
         <div className="title">
-          <div className="hide">
+          <StyledHide className="hide">
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide className="hide">
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide className="hide">
             <h2>true.</h2>
-          </div>
+          </StyledHide>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </StyledDescription>
+      <StyledImage className="image">
         <img src={home1} alt="Guy with a camera" />
-      </div>
+      </StyledImage>
     </StyledAbout>
   );
 };
@@ -40,6 +40,28 @@ const StyledAbout = styled.div`
   justify-content: space-between;
   padding: 5rem 10rem;
   color: white;
+`;
+
+const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const StyledImage = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const StyledHide = styled.div`
+  overflow: hidden;
 `;
 
 export default AboutSection;
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -25,6 +25,31 @@ button {
         color: white;
     }
 }
+
+h2 {
+    font-weight: lighter;
+    font-size: 4rem;
+}
+h3 {
+    color: white;
+}
+
+h4 {
+    font-weight: bold;
+}
+span {
+    font-weight: bold;
+    color: #23d997;
+}
+a {
+    font-size: 1.1.rem;
+}
+p {
+    padding: 3rem 0rem;
+    color: #ccc;
+    font-size: 1.4rem;
+    line-height: 150%;
+}
 `;
 
 export default GlobalStyle;
