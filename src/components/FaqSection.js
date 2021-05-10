3a98006d5e4cea6656da4336e90f5aed269dc23c
Matin Sarahi <matinsarahi@gmail.com>
2021-05-10 13:22:31 -0400

    movie info

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import {StyledAbout} from "../Styles";
+import { StyledAbout } from "../Styles";
 
-const Faq = styled(StyledAbout)`
+const StyledFaq = styled(StyledAbout)`
   display: block;
   span {
     display: block;
@@ -13,9 +13,9 @@ const Faq = styled(StyledAbout)`
   }
   .faq-line {
     background: #cccccc;
-      height: 0.2rem;
-      margin: 2rem 0rem;
-      width: 100%;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
   }
 
   .question {
@@ -33,60 +33,57 @@ const Faq = styled(StyledAbout)`
 const FaqSection = () => {
   return (
     <>
-      <Faq>
+      <StyledFaq>
         <h2>
           Any Questions? <span>FAQ</span>
         </h2>
-      <div className="question">
-        <h4>How do I start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam,
-            atque.
-          </p>
+        <div className="question">
+          <h4>How do I start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam,
+              atque.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam,
-            atque.
-          </p>
+        <div className="question">
+          <h4>Daily schedule</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam,
+              atque.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different payment methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam,
-            atque.
-          </p>
+        <div className="question">
+          <h4>Different payment methods</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam,
+              atque.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What products do you offer?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam,
-            atque.
-          </p>
+        <div className="question">
+          <h4>What products do you offer?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam,
+              atque.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      </Faq>
+      </StyledFaq>
     </>
   );
 };
-
-
-
 
 export default FaqSection;
